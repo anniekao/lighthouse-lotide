bb24ec3663b856ea1b656f96eff7faacb43035dc
Annie <anniekao@trentu.ca>
2019-07-23 17:32:33 +0000

    Applied eslint suggestions

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🤖Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -13,5 +13,5 @@ const eqArrays = function(arr1, arr2) {
     }
   }
   return true;
-}
+};
 
--- head.js
+++ head.js
@@ -14,4 +14,6 @@ const head = function(arr) {
   }
 };
 
+assertEqual(head(), []);
+
 
