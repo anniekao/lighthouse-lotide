9aa57c7bb841c88307e8b05f553e6a1b190da4cd
Annie <anniekao@trentu.ca>
2019-07-23 17:30:46 +0000

    Added completed function

---
+++ eqArrays.js
@@ -0,0 +1,17 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🤖Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💩Assertion Failed: ${ actual } !== ${ expected }`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1.length !== arr2.length || arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
