777e2e0d24b82d0559faf98a8d51db5ce1b88907
Annie <anniekao@trentu.ca>
2019-07-23 18:53:29 +0000

    Added complete function

---
+++ without.js
@@ -0,0 +1,26 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1.length !== arr2.length || arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✔✔✔ Array Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Array Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  return source.filter(x => !itemsToRemove.includes(x));
+};
+
+
