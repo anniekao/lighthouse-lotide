2fe284f6f1588c6bda0ac54f8daeaeba418a3a34
Annie <anniekao@trentu.ca>
2019-07-23 20:19:27 +0000

    Added completed flatten function

---
+++ flatten.js
@@ -0,0 +1,18 @@
+const flatten = function(arr) {
+  // simplified answer with .reduce(): return arr.reduce((arr, ele) => arr.concat(ele), []);
+
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        result.push(arr[i][j]);
+      }
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+
+
+
