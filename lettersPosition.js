853538396578b16fd9a0f10131906cf4e60d6111
Annie <anniekao@trentu.ca>
2019-07-24 15:16:42 +0000

    Added completed lettersPosition function

---
+++ lettersPosition.js
@@ -0,0 +1,40 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1.length !== arr2.length || arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✔✔✔ Array Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Array Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const letterPosition = function(str) {
+  const countIndex = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (countIndex[str[i]]) {
+      countIndex[str[i]].push(i);
+    } else {
+      countIndex[str[i]] = [];
+      countIndex[str[i]].push(i);
+    }
+  }
+
+  return countIndex;
+};
+
+
+
+
