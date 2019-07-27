9f95ed23b1d12ac8e631bb4d53f8669cfcba8b6e
Annie <anniekao@trentu.ca>
2019-07-27 23:59:58 +0000

    Debugged

--- eqObjects.js
+++ eqObjects.js
@@ -11,7 +11,11 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1.length !== arr2.length || arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (eqArrays(arr1[i], arr2[i]) === false) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
@@ -26,15 +30,45 @@ const eqObjects = function(obj1, obj2) {
   for (let ele in obj1) {
     if (obj1.hasOwnProperty(ele) && obj2.hasOwnProperty(ele)) {
       if (Array.isArray(obj1[ele]) && Array.isArray(obj2[ele])) {
-        if (!eqArrays(obj1[ele], obj2[ele])) {
-          return false;
-        }
-      } else if (obj1[ele] !== obj2[ele]) {
-        return false;
+        return eqArrays(obj1[ele], obj2[ele]);
       }
+    } else if (obj1[ele] !== obj2[ele]) {
+      return false;
     }
   }
+  
   return true;
+  
 };
 
+// assertEqual Tests
+// assertEqual("Lighthouse Labs", "Bootcamp"); // Fail
+// assertEqual(1, 1); // Pass
 
+// eqArrays Tests
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+// // EqObjects Tests
+// eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }); // => true
+// eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }); // => false
+// eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }); // => false
+
+
+// Tests
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3], e: "4" };
+console.log(eqObjects(cd, cd2)); // => false
+
+const dc2 = { d: ["2", 3], c: "1", e:[5, 6, 7] };
+console.log(eqObjects(dc, dc2)); // => false
+
+const ef = {e: "7", f: [4, 28, 29], g: "!"};
+const ef2 = {e: "7", f: [4, 28, 29], g: "!"};
+console.log(eqObjects(ef, ef2)); // => true
