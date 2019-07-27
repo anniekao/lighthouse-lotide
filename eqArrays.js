a4e729597013624396d9fff243079b13b0c50ce0
Annie <anniekao@trentu.ca>
2019-07-27 23:35:08 +0000

    Modified function to handle nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -11,10 +11,24 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1.length !== arr2.length || arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (eqArrays(arr1[i], arr2[i]) === false) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
 };
 
+// Tests non-nested arrays
+console.log(eqArrays([2, 3], [4, 5])); // false
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // true
+
+// Tests nested arrays
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // true
+console.log(eqArrays([[2, 3], [4, 7]], [[2, 3], [4, 5]])); // false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
