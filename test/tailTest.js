092b571f8283d65809d472ca6903d199ad1d647d
Annie <anniekao@posteo.net>
2019-07-29 08:55:33 -0400

    Refactored tests to incorporate mocha/chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,21 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
+const assertArraysEqual = require('../assertArraysEqual');
 
+describe("#tail", () => {
+  it("returns [1, 2, 3, 4] for [0, 1, 2, 3, 4]", () => {
+    assert.deepEqual(tail([0, 1, 2, 3, 4]), [1, 2, 3, 4]);
+  });
 
-assertArraysEqual(tail([0, 1, 2, 3, 4]), [1, 2, 3, 4]);
-assertArraysEqual(tail([2, 3, 4]), [3, 4]);
-assertArraysEqual(tail([3, 4]), [4]);
-assertArraysEqual(tail(["a", "b", "c", "d"]), ["b", "c", "d"]);
+  it("returns [3, 4] for [2, 3, 4]", () => {
+    assert.deepEqual(tail([2, 3, 4]), [3, 4]);
+  });
+
+  it("returns [4] for [3, 4]", () => {
+    assert.deepEqual(tail([3, 4]), [4]);
+  });
+
+  it("returns ['b', 'c', 'd'] for ['a', 'b', 'c', 'd']", () => {
+    assert.deepEqual(tail(['a', 'b', 'c', 'd']), ['b', 'c', 'd']);
+  });
+});
