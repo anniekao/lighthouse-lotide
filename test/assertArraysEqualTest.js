4891df5511ce424ab1fa1d88ac74a1afb7e136f0
Annie <anniekao@posteo.net>
2019-07-29 09:28:41 -0400

    Modified tests

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,8 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const eqArrays = require('../eqArrays');
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertArraysEqual(eqArrays([3, 6, 9], [2, 4, 6]), false);
-assertArraysEqual(eqArrays([5, 10, 15, 28], [5, 10, 15, 28]), true);
-assertArraysEqual(eqArrays(["a", "x", "l"], ["x", "o", "x", "o"]), false);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([3, 6, 9], [2, 4, 6]);
+assertArraysEqual([5, 10, 15, 28], [5, 10, 15, 28]);
+assertArraysEqual(["a", "x", "l"], ["x", "o", "x", "o"]);
+
